a56a8eca22fecad6d3646cd72ca574618bd4e734
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-11-30 11:39:45 +0100

    remove a char at index position in a string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/pyhton3
 def remove_char_at(str, n):
-    str.pop(n)
-    print(str)
+    if n < len(str) and n >= 0:
+        popped = str[:n] + str[n+1:]
+        return popped
+    else:
+        return str
