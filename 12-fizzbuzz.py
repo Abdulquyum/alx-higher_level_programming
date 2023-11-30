6733403df0349e63ce910d77d0b052368a251e05
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-11-30 03:23:07 +0100

    FizzBuzz for multiples of both 5 and 3, and each words seperately for each multiles

--- 12-fizzbuzz.py
+++ 12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for num in range(1, 101):
         if num % 15 == 0:
-            print("FizzBuzz".format(), end=' ')
+            print("FizzBuzz", end=' ')
         elif num % 5 == 0:
-            print ("Buzz".format(), end=' ')
+            print ("Buzz", end=' ')
         elif num % 3 == 0:
-            print("Fizz".format(), end=' ')
+            print("Fizz", end=' ')
         else:
             print(num, end=' ')
