6270c2698965c9849f7ef77dcd98b863852663c5
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-05 04:39:38 +0100

    remove all occurence of 'c' and 'C' in the string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 def no_c(my_string):
+    no_c = ''
     for c in my_string:
-        if c == 'c' or c == 'C':
-            my_string.remove(c)
-    return my_string
+        if c != 'c' and c != 'C':
+            no_c += c
+    return no_c
