5651081ada6bf5cb7b7c7c3df4e0765b2f521209
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-05 04:22:37 +0100

    replace an element in a string and keep the original

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     temp = my_list.copy()
-    if idx < 0 or idx > (len(my_list) - 1):
-        return my_list
-    else:
+    if 0 <= idx < len(my_list):
         temp[idx] = element
         return temp
-    return my_list
+    else:
+        return my_list
