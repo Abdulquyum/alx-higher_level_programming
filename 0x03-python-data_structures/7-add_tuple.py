b1a91eee0c27f4a8fe00b80d4b623728099e7f03
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-06 11:09:21 +0100

    print matrix in a string format

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python#
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_c = ()
+    if len(tuple_a) < 2:
+        tulple_a[1] = 0
+    a = tuple_a[0] + tuple_b[0]
+    b = tuple_a[1] + tuple_b[1]
+
+    tuple_c.append(a)
+    tuple_c.append(b)
