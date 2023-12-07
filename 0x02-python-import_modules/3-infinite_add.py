6bd7f05e2b412c7d63f71ef0dd71ff23600e4363
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-07 02:58:11 +0100

    adds argument inifinitely

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-result = 0
-x = 1
-while x < len(sys.argv):
-    result += int(sys.argv[x])
-    x += 1
-print("{}".format(result))
+if __name__ == "__main__":
+    import sys
+    result = 0
+    x = 1
+    while x < len(sys.argv):
+        result += int(sys.argv[x])
+        x += 1
+    print("{}".format(result))
