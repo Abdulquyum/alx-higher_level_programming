a3160f7f638761af1f3c0290c2bda77c992de776
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-07 04:44:05 +0100

    replace element of a lis and rearrange in a new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+def search_replace(my_list, search, replace):
+    new_list = []
+    for x in my_list:
+        if x == search:
+            x = replace
+        new_list.append(x)
+    return new_list
