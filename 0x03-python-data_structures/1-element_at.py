7ddbbb155d087c41adbdb2c1ea51967a194c3ad6
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-17 18:34:56 +0100

    retrieve elemnt at index from the list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,5 +2,4 @@
 def element_at(my_list, idx):
     if 0 <= idx < len(my_list):
         return (my_list[idx])
-    else:
-        return None
+    return None
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
-        print(" ".join(map(str, x)))
+        print(" ".join("{:d}".format(x) for x in matrix))
