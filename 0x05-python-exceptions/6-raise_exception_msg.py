5f3243d280564b798d06a611b6ccb9973fc70886
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-19 05:26:03 +0100

    raised exception with a display message

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/bin/usr/python3
 def raise_exception_msg(message=""):
-    try:
-        raise NameError
-    except NameError:
-        ("{:s}".format(message))
+        raise NameError(message)
