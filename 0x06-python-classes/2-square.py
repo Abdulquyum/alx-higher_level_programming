5c73dafb5daa9881252a47ddb1d63e1073f1349f
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-12-22 02:29:36 +0100

    more methods to square class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+
+""" class defines a square """
+
+
 class Square:
+    """contains a module with definition """
+
     def __init__(self, size=0):
+        """inside module checking if size is of int type
+            or if size is less than 0 """
         self.__size = size
         try:
             if type(size) is not int:
