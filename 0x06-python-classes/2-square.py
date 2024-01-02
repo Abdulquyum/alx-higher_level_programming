ecc49c90b7985ecf4aa4fac85970748df99d35f4
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-02 17:18:37 +0100

    A class that defines a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,9 +10,9 @@ class Square:
         """inside module checking if size is of int type
             or if size is less than 0 """
         self.__size = size
-        try:
-            if type(size) is not int:
-                raise TypeError("size must be an integer")
-        except Exception:
-            if size < 0:
-                raise ValueError("size must be >= 0")
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
