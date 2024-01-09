d1f929219e6f55fe75105d7adfd99cdcf31e46a0
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-09 05:24:20 +0100

    A defined BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" define BaseGeometry class """
+
+
+class BaseGeometry(object):
+    """ A defined method of BaseGeometry """
+    def area(self):
+        raise Exception("area() is not implemented")
