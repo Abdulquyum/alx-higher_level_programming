6b3dc871e104a67a897766b32c506d344a50b314
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-10 14:55:33 +0100

    check if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" check for object if its a class or instance """
 def def is_kind_of_class(obj, a_class):
     """ check if the object is an instance of, or
         if the object is an instance of a class that inherited from """
