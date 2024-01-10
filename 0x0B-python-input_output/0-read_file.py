87b318a73a3878ae2f5f3a3c2e837e0cd3ab6f90
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-10 12:30:48 +0100

    a script to read from a file and prints to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,6 @@ def read_file(filename=""):
     """ open and reads file and print content """
     with open(filename, 'r', encoding="utf-8") as q:
         read_file = q.read()
-    print(read_file)
+        if read_file is None:
+            q.close()
+    print(read_file, end="")
