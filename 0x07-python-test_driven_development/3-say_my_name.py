0fd05052395db1e6409d39f1a262d848cee58e5a
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-10 22:23:57 +0100

    prints full name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" function that prints name """
+
+
 def say_my_name(first_name, last_name=""):
+    ''' Module that help to print full name '''
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
