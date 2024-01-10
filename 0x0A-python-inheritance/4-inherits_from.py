eacb820e7a6b1a143ca80f37f100a7491e04a991
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-10 15:02:55 +0100

    if the object is a subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" check if the object is a subclass """
+
+
 def inherits_from(obj, a_class):
+    """  if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class """
     if issubclass(type(obj), a_class):
         return True
     else:
