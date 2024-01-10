ec35c1c42cb9482349668ff3c7ff5be434a295b7
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-10 23:21:48 +0100

    function that adds two integers with its doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,7 +7,7 @@ def add_integer(a, b=98):
     if not isinstance(a, int):
         raise TypeError("a must be an integer")
     if not isinstance(b, int):
-       raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
