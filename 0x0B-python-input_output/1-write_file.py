69c42048255d48cd9c73ffe0352f5956fb43815a
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-10 12:42:00 +0100

    write to to file and return number of char written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" write to file """
+
+
 def write_file(filename="", text=""):
+    """ open and write some texts to file """
     with open(filename, 'w', encoding="utf-8") as w:
-        return w.write(text)
+        write_file = w.write(text)
+    return write_file
