7c6f45c406dc01c84f7939218958695bf30434b5
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-15 21:52:36 +0100

    a function that return the maximum integer in a lsi and its unit test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,13 +3,13 @@ def matrix_divided(matrix, div):
     new_matrix = []
     for row in matrix:
         for col in matrix:
-            if not isinstance(col, int) or not isinstance(col, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            #if not isinstance(col, int) or not isinstance(col, float):
+                #raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             #elif len(row) != len(col):
                 #raise TypeError("Each row of the matrix must have the same size")
-            elif not isinstance(div, int) or not isinstance(div, float):
-                raise TypeError("div must be a number")
-            elif div == 0:
-                raise ZeroDivisionError("division by zero")
-            new_matrix.append("{:.2f}".format(col / div))
-        return new_matrix
+            #elif not isinstance(div, int) or not isinstance(div, float):
+                #raise TypeError("div must be a number")
+            #elif div == 0:
+                #raise ZeroDivisionError("division by zero")
+            new_matrix.append(col / div)
+    return new_matrix
