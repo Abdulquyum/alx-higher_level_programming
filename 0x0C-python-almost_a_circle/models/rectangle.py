3836b6571afe75567c36573f730d0a3b04c5f9d3
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-15 02:48:02 +0100

    set instances for rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,3 +14,43 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         Base.__init__(self, id)
+
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be integer".format(self.__width))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(self.__width))
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(self.__height))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(self.__height))
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, value):
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(self.__x))
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, value):
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(self.__y))
+        self.__y = value
