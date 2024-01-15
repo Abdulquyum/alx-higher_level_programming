77dab14c8a3138a08c603e32a28ed159ffb71858
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-15 21:57:52 +0100

    a function that return the maximum integer in a lsi and its unit test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,6 +9,8 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([5, 0, 2, 1]), 5)
+        self.assertEqual(max_integer([2, 4, 9, 6, 8]), 9)
         self.assertNotEqual(max_integer([1, 3, 4, 2]), 2)
 
 if __name__ == "__main__":
