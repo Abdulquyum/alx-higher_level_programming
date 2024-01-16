3e7a4267cd8bdd8cebbbb02e4d51a13551d9832c
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-16 04:49:05 +0100

    Rectangle with instances and getter and setter functions

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,24 +9,12 @@ from models.base import Base
 class Rectangle(Base):
     """ modules to define rectangle inheriting from Base """
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__height = height
-        self.__width = width
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
+    
     @property
     def width(self):
         return self.__width
@@ -38,6 +26,17 @@ class Rectangle(Base):
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
