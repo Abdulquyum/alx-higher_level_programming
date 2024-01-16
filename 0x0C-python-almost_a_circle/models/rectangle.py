1a49d0d51d35e9ee5b9c8488837191190bd8bc86
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-16 11:53:24 -0800

    Complete Rectangle with instances, getter and setter function, diplays, print, return area and inherits from base, updated with arg and kwargs function

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,18 +9,20 @@ from models.base import Base
 class Rectangle(Base):
     """ modules to define rectangle inheriting from Base """
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
     def width(self):
+        ''' width getter function '''
         return self.__width
 
     @width.setter
     def width(self, value):
+        ''' width getter function '''
         if not isinstance(value, int):
             raise TypeError("width must be integer")
         if value <= 0:
@@ -29,10 +31,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        ''' height getter module '''
         return self.__height
 
     @height.setter
     def height(self, value):
+        ''' height setter module '''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -41,28 +45,35 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        ''' x getter func '''
         return self.__x
 
     @x.setter
     def x(self, value):
+        ''' x setter func '''
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
+        ''' y getter mod '''
         return self.__y
 
     @y.setter
     def y(self, value):
+        ''' y setter mod '''
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
+        ''' returns the area of the rectangle '''
         return self.__height * self.__width
 
     def display(self):
+        ''' Display rectangle with '#' representation
+            considering x and y '''
         for a in range(self.__x):
             print()
         for b in range(self.__height):
@@ -73,10 +84,13 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
+        ''' prints str representation '''
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
+        """ updates rectangle with the aid of
+        args and kwargs """
         if len(args) > 0:
             try:
                 self.id = args[0]
