53db30549c1f4a56d9d7e10b21e7a7f235c52cf2
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-01-19 12:13:00 +0100

    Square in the amking

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,9 +5,12 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ consists of Module that amkes square"""
     def __init__(self, size, x=0, y=0, id=None):
+        """ initialization of inherited factors """
         super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
+        """ prints string representation """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
