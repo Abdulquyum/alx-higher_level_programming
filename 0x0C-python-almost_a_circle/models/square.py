521b21c6fb9c608b459c6db3be2a8d12f70f4ae3
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-03-24 18:01:51 +0100

    Square design

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,3 +15,62 @@ class Square(Rectangle):
         """ prints string representation """
         return "[Square] ({}) {}/{} - {}".format(
                 self.id, self.x, self.y, self.size)
+    @property
+    def width(self):
+        ''' width getter function '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        ''' width getter function '''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
+
+    @property
+    def height(self):
+        ''' height getter module '''
+        return self.__height
+
+    
+    @height.setter
+    def height(self, value):
+        ''' height setter module '''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+        self.__width = value
+
+
+    @property
+    def x(self):
+        ''' x getter func '''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        ''' x setter func '''
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        ''' y getter mod '''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        ''' y setter mod '''
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
