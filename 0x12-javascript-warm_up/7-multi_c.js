1107e0fe807da5be2eab6af1b7bd5f9cd9db9c6f
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-09 11:51:46 +0100

    prints C is fun in argument passed tiome

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
+
 let i;
-for (i = 0; i < argv[2]; i++) {
-	console.log("C is fun");
+const a = parseInt(argv[2]);
+if (!isNaN(a)) {
+  for (i = 0; i < a; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
