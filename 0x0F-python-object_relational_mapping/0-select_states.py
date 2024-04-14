6fa942855a92f3efd6fd9ff50efec4f9080f0034
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-14 13:50:47 +0100

    lists all states from the database hbtn_0e_0_usa using ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM hbtn_0e_0_usa.states ORDER BY states.id")
+    cur.execute("SELECT * FROM states ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
 	    print(row)
