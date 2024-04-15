7a2245b39c992a1061ce918652c529ffbed4f0d7
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-15 04:56:59 +0100

    class definition of state, inherit Base with MySQLAlchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+
 """ define state and inherits from BAse class """
 
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "hbtn_0e_0_usa"), pool_pre_ping=True)
-Base= declarative_base()
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+        format("root", "root", "hbtn_0e_0_usa"), pool_pre_ping=True)
+Base = declarative_base()
+
 
 class state(Base):
     """ state class with instances """
     __tablename__ = "states"
     id = Column(Integer, unique=True, autoincrement=True, primary_key=True)
     name = Column(String(128))
+
 
 Base.metadata.create_all(engine)
