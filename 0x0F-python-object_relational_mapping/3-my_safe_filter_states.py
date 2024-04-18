f512cd021135982eaa9acff2fc17ebc0cf48c3a3
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-18 14:43:28 +0100

    displays all values in the states table of hbtn_0e_0_usa where name matches the argument and safe from injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" displays all values in the states table of hbtn_0e_0_usa
+    where name matches the argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+    query = ("SELECT * FROM states WHERE name='{}' ORDER BY states.id".
+             format(sys.argv[4]))
+    cur.execute(query)
+    [print(row) for row in cur.fetchall()]
