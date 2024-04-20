2fda0c316495e3cdd33cf39e374b4d0cb6aecd46
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-20 15:06:52 +0100

    lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
