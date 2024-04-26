8ed815125108b356d9f7a5bf9d85eb9d76af03a9
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-26 23:22:51 +0100

    takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #  takes in a URL, sends a GET request to the URL, and displays the body of the response
-url="$1"
-response_body=$(mktemp)
-curl -s -o "$response_body" -w "%{http_code}" "$url" > /dev/null
+curl -s -o response_body -w "%{http_code}" $1 > /dev/null
