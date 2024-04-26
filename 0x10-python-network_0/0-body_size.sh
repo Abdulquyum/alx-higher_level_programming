bb724e63a60cbb7b44fb547f044c12ea4698d5f1
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-26 02:06:17 +0100

    takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
 url="$1"
-curl -s -o ${url} | wc -c
+response_body=$(mktemp)
+curl -s -o "$response_body" "$url" | wc -c
