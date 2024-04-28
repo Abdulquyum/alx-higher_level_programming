71189f54d5146a32be0bfa64dcc4b9e0cbf63715
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-29 00:23:55 +0100

    sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,7 +11,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]}
+    email = sys.argv[2]
 
     req = requests.post(url, data=email)
     body = req.content
