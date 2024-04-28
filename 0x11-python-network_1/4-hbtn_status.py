f20fe59eb8c8769679147394a7706565740fa442
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-28 23:21:22 +0100

    Fteches https://alx-intranet.hbtn.io/status using request library

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ import requests
 data = requests.get('https://alx-intranet.hbtn.io/status')
 body = data.content.decode('utf-8')
 print("Body response:")
-print("\t- type: ", type(body))
-print("\t- content: ", body)
+print(f"\t- type: {type(body)}")
+print(f"\t- content: {str(body)}")
