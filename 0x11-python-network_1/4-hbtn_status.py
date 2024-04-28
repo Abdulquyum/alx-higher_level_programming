b8617c0416ae65cfa713fdef2558aa09cfd05836
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-28 23:14:01 +0100

    Fteches https://alx-intranet.hbtn.io/status using request library

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status
+Using reequest pyhton library
+"""
+
+import requests
+
+
+data = requests.get('https://alx-intranet.hbtn.io/status')
+body = data.content
+print("Body response:")
+print("\t- type: ", type(body))
+print("\t- content: ", body)
