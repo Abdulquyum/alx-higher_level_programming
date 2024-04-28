62c49f6aa30203348c7e8f095430bfa754a3de94
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-28 23:52:52 +0100

    accept URL and fetches an header X-Request-Id value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' sends a request to the URL and displays,
+''' sends a request to URL and displays,
 the value of the variable X-Request-Id in the response header
 Using responses library in python
 '''
