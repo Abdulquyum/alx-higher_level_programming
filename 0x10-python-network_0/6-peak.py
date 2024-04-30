8780b92c2074940d199a805a56fa55405aedd52b
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-05-01 00:53:54 +0100

    Function to check for peak or highest number in a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,8 +10,5 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
 
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-    
     list_of_integers.sort()
-    return(list_of_integers[-1])
+    return (list_of_integers[-1])
