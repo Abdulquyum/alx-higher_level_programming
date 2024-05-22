7f4b45b73bc6a8a698f19c75c3fe8695bdfb02fc
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-05-22 06:09:13 +0100

    read file using fs module

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
+
+const { argv } = require('process');
+const file = argv[2]
 const fs = require('fs');
 
-fs.readFile('cisfun', 'utf8', (err, data) => {
+fs.readFile(file, 'utf8', (err, data) => {
 	  if (err) {
 		      console.error(err);
 		      return;
